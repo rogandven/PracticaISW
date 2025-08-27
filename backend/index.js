8fa2dc7b79a32d24ce51fa9accb3a2e728f99321
Roger Andrés Venegas Opazo <roger.venegas2301@alumnos.ubiobio.cl>
2025-08-27 15:36:23 -0400

    Unfinished example

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,13 @@
 import express from "express";
 // const express = require('express')
-const app = express()
-const port = 3000
+const app = express();
+app.use(express.json());
+const port = 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('Hello World!');
 })
 
 app.listen(port, () => {
-    console.log(`Corriendo en https://localhost:${port}`)
+    console.log(`Corriendo en https://localhost:${port}`);
 })
